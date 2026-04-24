af4fedb9f8dccff816a98bd914ce3a2801da56b4
Shohjahon <shohjahon0192@gmail.com>
2026-04-24 22:58:30 +0500

    feat: build premium cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { CartService } from '../../core/services/cart.service';
 
 @Component({
   selector: 'app-cart',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
-  readonly cartService = new CartService();
-}
+  readonly cartService = inject(CartService);
+} 
