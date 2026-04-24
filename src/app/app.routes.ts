14a754aa51307a2838d74164a67af31d34c99078
Shohjahon <shohjahon0192@gmail.com>
2026-04-24 22:36:23 +0500

    feat: add main layout with navigation and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            {
+                path: '',
+                loadComponent: () =>
+                    import('./pages/home/home.component').then(m => m.HomeComponent)
+            },
+            {
+                path: 'products',
+                loadComponent: () =>
+                    import('./pages/products/products.component').then(m => m.ProductsComponent)
+            },
+            {
+                path: 'cart',
+                loadComponent: () =>
+                    import('./pages/cart/cart.component').then(m => m.CartComponent)
+            },
+            {
+                path: 'checkout',
+                loadComponent: () =>
+                    import('./pages/checkout/checkout.component').then(m => m.CheckoutComponent)
+            }
+        ]
+    }
+];
